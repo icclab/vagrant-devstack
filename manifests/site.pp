85899860d4ec330d3e32add4983e1f174b70a17d
Xiao Hanyu <xiaohanyu1988@gmail.com>
2013-05-21 11:09:40 +0800

    fix some indentation issues

--- manifests/site.pp
+++ manifests/site.pp
@@ -16,60 +16,60 @@
 node /^devstack/ {
 
 
-	#set if you want to ######
-	$trema = false
-	##########################
-	
-	if $trema {
-		# enter puppet code
-		$source = "https://github.com/nec-openstack/devstack-quantum-nec-openflow.git"
-	}
-	else {
-		$source = "https://github.com/openstack-dev/devstack.git"
-	}
+  #set if you want to ######
+  $trema = false
+  ##########################
 
-	#ensure git is installed
-	package { 'git':
-		ensure 		=> 'present',
-	}
+  if $trema {
+	# enter puppet code
+	$source = "https://github.com/nec-openstack/devstack-quantum-nec-openflow.git"
+  }
+  else {
+	$source = "https://github.com/openstack-dev/devstack.git"
+  }
 
-	# clone the devstack repo
-	vcsrepo { "/home/vagrant/devstack":
-		ensure 		=> present,
-		provider 	=> git,
-		source 		=> $source,
-		user 		=> 'vagrant',
-		require 	=> Package["git"],
-	}
+  #ensure git is installed
+  package { 'git':
+	ensure 		=> 'present',
+  }
 
-# , http://fedorapeople.org/groups/heat/prebuilt-jeos-images/F16-x86_64-cfntools.qcow2,http://fedorapeople.org/groups/heat/prebuilt-jeos-images/F16-i386-cfntools.qcow2'"
-# ENABLED_SERVICES+=,heat,h-api,h-api-cfn,h-api-cw,h-eng
-	$localrc_cnt = "
-ADMIN_PASSWORD=admin
-MYSQL_PASSWORD=admin
-RABBIT_PASSWORD=admin
-SERVICE_PASSWORD=admin
-SERVICE_TOKEN=admin
-APACHE_USER=vagrant
-API_RATE_LIMIT=False
-HOST_IP=10.1.2.44
-FLOATING_RANGE=10.1.2.224/27
-IMAGE_URLS+='http://uec-images.ubuntu.com/precise/current/precise-server-cloudimg-amd64-disk1.img'"
+  # clone the devstack repo
+  vcsrepo { "/home/vagrant/devstack":
+	ensure 		=> present,
+	provider 	=> git,
+	source 		=> $source,
+	user 		=> 'vagrant',
+	require 	=> Package["git"],
+  }
 
-    file { "/home/vagrant/devstack/localrc":
-      content 	=> "$localrc_cnt",
-      require 	=> Vcsrepo["/home/vagrant/devstack"],
-      group		=> "vagrant",
-      owner		=> "vagrant",
-    }
+  # , http://fedorapeople.org/groups/heat/prebuilt-jeos-images/F16-x86_64-cfntools.qcow2,http://fedorapeople.org/groups/heat/prebuilt-jeos-images/F16-i386-cfntools.qcow2'"
+  # ENABLED_SERVICES+=,heat,h-api,h-api-cfn,h-api-cw,h-eng
+  $localrc_cnt = "
+  ADMIN_PASSWORD=admin
+  MYSQL_PASSWORD=admin
+  RABBIT_PASSWORD=admin
+  SERVICE_PASSWORD=admin
+  SERVICE_TOKEN=admin
+  APACHE_USER=vagrant
+  API_RATE_LIMIT=False
+  HOST_IP=10.1.2.44
+  FLOATING_RANGE=10.1.2.224/27
+  IMAGE_URLS+='http://uec-images.ubuntu.com/precise/current/precise-server-cloudimg-amd64-disk1.img'"
 
-	#run stack.sh as current user (vagrant)
-	exec { "/home/vagrant/devstack/stack.sh":
-		cwd     	=> "/home/vagrant/devstack",
-		group		=> "vagrant",
-		user		=> "vagrant",
-		logoutput	=> on_failure,
-		timeout		=> 0, # stack.sh takes time!
-		require 	=> File["/home/vagrant/devstack/localrc"],
-	}
+  file { "/home/vagrant/devstack/localrc":
+    content 	=> "$localrc_cnt",
+    require 	=> Vcsrepo["/home/vagrant/devstack"],
+    group		=> "vagrant",
+    owner		=> "vagrant",
+  }
+
+  #run stack.sh as current user (vagrant)
+  exec { "/home/vagrant/devstack/stack.sh":
+	cwd     	=> "/home/vagrant/devstack",
+	group		=> "vagrant",
+	user		=> "vagrant",
+	logoutput	=> on_failure,
+	timeout		=> 0, # stack.sh takes time!
+	require 	=> File["/home/vagrant/devstack/localrc"],
+  }
 }
